9a7d9951d045f3262478ea6d0decce9dc2904e03
Shota <eg.shota@yandex.ru>
2024-06-18 19:41:49 +0300

    project v.1
    playable version
    
    **future update
    -edit background
    -edit menu
    -add unityAds

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,15 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    private float horizontalInput;
-    private float moveSpeed = 10f;
+    private float _horizontalInput;
+    private float _moveSpeed = 10f;
+    private bool _isFacingRight = true;
 
+    private Animator _animator;
     private Rigidbody2D rigidbody2D;
 
     public event UnityAction PlayerDied;
@@ -14,16 +17,30 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
+        _horizontalInput = Input.GetAxis("Horizontal");
+        FlipSprite();
     }
 
     private void FixedUpdate()
     {
-        rigidbody2D.velocity = new Vector2(horizontalInput * moveSpeed, rigidbody2D.velocity.y);
+        rigidbody2D.velocity = new Vector2(_horizontalInput * _moveSpeed, rigidbody2D.velocity.y);
+        _animator.SetFloat("xVelocity", Math.Abs(rigidbody2D.velocity.x));
+    }
+
+    private void FlipSprite()
+    {
+        if (_isFacingRight && _horizontalInput < 0f || !_isFacingRight && _horizontalInput > 0f)
+        {
+            _isFacingRight = !_isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x = -1f;
+            transform.localScale = localScale;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
